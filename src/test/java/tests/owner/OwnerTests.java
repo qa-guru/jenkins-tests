6942e1cd38b708fec113ebd9875ffb7c12c6b6f4
svasenkov <aanher@gmail.com>
2022-02-18 20:36:40 +0300

    added owner task

--- src/test/java/tests/owner/OwnerTests.java
+++ src/test/java/tests/owner/OwnerTests.java
@@ -2,6 +2,7 @@ package tests.owner;
 
 import config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
@@ -10,6 +11,7 @@ public class OwnerTests {
     CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @Test
+    @Tag("owner")
     void loginTest() {
         String login = config.login();
         String password = config.password();
